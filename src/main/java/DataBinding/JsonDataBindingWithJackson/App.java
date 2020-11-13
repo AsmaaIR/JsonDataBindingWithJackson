dc64648b0b149fa4debdd5f7ac41f341ab56bc66
AsmaaIR <asmaaibrahim06@gmail.com>
2020-11-13 20:51:04 +0200

    init commit

---
+++ src/main/java/DataBinding/JsonDataBindingWithJackson/App.java
@@ -0,0 +1,13 @@
+package DataBinding.JsonDataBindingWithJackson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
